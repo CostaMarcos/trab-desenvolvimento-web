caefebd69806458c5f3d90a6fbde9e9cfbfb87f4
Lucas Castro <lucasacastro99@gmail.com>
2021-08-15 19:29:26 -0400

    feat(curso): adição dos modelos e controladores

--- src/server.js
+++ src/server.js
@@ -7,9 +7,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const turma = require('./controllers/turmaController');
+const cursoFic = require('./controllers/cursoFicController')
 
 app.use('/turma', turma);
+app.use('/cursoFic', cursoFic)
 
 
 app.listen(3000);
-
