0d81e09e540114435654692b28e147e453bc249c
CarloGabriel <86427272+CarloGabriel@users.noreply.github.com>
2021-08-16 20:24:52 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,10 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const turma = require('./controllers/turmaController');
 const cursoFic = require('./controllers/cursoFicController');
 const aluno = require('./controllers/alunoController');
+const professor = require('./controllers/professorController');
 
 app.use('/turma', turma);
 app.use('/cursoFic', cursoFic)
 app.use('/aluno', aluno)
+app.use('/professor', professor)
 
 
 app.listen(3000);
